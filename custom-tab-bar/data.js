e7c2ba310b98f75f4ad3b74f4719c5731479b7ea
ChenQingze <chenqingze107@gmail.com>
2023-10-19 21:28:02 +0800

    confirm product and variant design

--- custom-tab-bar/data.js
+++ custom-tab-bar/data.js
@@ -5,6 +5,11 @@ export default [
     url: 'pages/product/list/index',
   },
   {
+    icon: 'cart',
+    text: '购物车',
+    url: 'pages/cart/index',
+  },
+  {
     icon: 'order-ascending',
     text: '订单',
     url: 'pages/order/index',
---
+++ pages/cart/components/cart-item/index.js
@@ -0,0 +1,17 @@
+// pages/cart/components/cart-item/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {},
+
+  /**
+   * Component initial data
+   */
+  data: {},
+
+  /**
+   * Component methods
+   */
+  methods: {},
+});
